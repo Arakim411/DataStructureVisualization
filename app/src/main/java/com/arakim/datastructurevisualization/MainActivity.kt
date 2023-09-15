5980d45741e5aff297e1f7c45a9d7312d2035d03
barteksawicki <bartlomiej.sawicki@lamin.ar>
2023-09-15 13:11:52 +0200

    feat: setup theme

--- app/src/main/java/com/arakim/datastructurevisualization/MainActivity.kt
+++ app/src/main/java/com/arakim/datastructurevisualization/MainActivity.kt
@@ -4,40 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.arakim.datastructurevisualization.ui.theme.DataStructureVisualizationTheme
+import com.arakim.dataStructureVisualization.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DataStructureVisualizationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+            AppTheme {
+                App()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Hello World!")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    DataStructureVisualizationTheme {
-        Greeting("Android")
+fun AppLightThemePreview() {
+    AppTheme(useDarkTheme = false) {
+        App()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppDarkThemePreview() {
+    AppTheme(useDarkTheme = true) {
+        App()
     }
 }
