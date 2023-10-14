9f7765d4c9b931bdbd3c7c887d10c9ea8821d16f
barteksawicki <bartlomiej.sawicki@lamin.ar>
2023-10-14 09:14:24 +0200

    feat: move VisualizationEngine module inside VisualizationBuilder

--- ui/visualization-engine/src/main/kotlin/com/arakim/datastructurevisualization/ui/visualizationEngine/presenter/model/VisualizationElement.kt
+++
@@ -1,8 +0,0 @@
-package com.arakim.datastructurevisualization.ui.visualizationEngine.presenter.model
-
-import androidx.compose.ui.unit.DpOffset
-
-data class VisualizationElement(
-    val title: String,
-    val position: DpOffset,
-)
