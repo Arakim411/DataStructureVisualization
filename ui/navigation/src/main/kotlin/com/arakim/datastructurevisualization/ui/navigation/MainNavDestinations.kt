123a26418b14fc62fe3526c1340719424a7f00be
barteksawicki <bartlomiej.sawicki@lamin.ar>
2023-11-13 10:03:33 +0100

    feat(Navigation): update navigation routes

--- ui/navigation/src/main/kotlin/com/arakim/datastructurevisualization/ui/navigation/MainNavDestinations.kt
+++ ui/navigation/src/main/kotlin/com/arakim/datastructurevisualization/ui/navigation/MainNavDestinations.kt
@@ -2,26 +2,20 @@ package com.arakim.datastructurevisualization.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import com.arakim.datastructurevisualization.ui.navigation.MainNavDestinations.ChooseDataStructureDestination
+import com.arakim.datastructurevisualization.ui.navigation.MainNavDestinations.DeletedDestination
+import com.arakim.datastructurevisualization.ui.navigation.MainNavDestinations.FavoritesDestination
+
 
 enum class MainNavDestinations(val route: String) {
-    BinarySearchTree("binarySearchTree"),
-    Stack("stack"),
-    Queue("queue"),
-    LinkedList("linkedList"),
-    HashTable("hashTable"),
-
-    Favorite("favorite"),
-    Delete("delete"),
-
+    ChooseDataStructureDestination("choose_data_structure"),
+    FavoritesDestination("favorites"),
+    DeletedDestination("deleted")
 }
 
 @Composable
 fun MainNavDestinations.toStringResources(): String = when (this) {
-    MainNavDestinations.BinarySearchTree -> stringResource(id = R.string.destination_name_binary_search_tree)
-    MainNavDestinations.Stack -> stringResource(id = R.string.destination_name_stack)
-    MainNavDestinations.Queue -> stringResource(id = R.string.destination_name_queue)
-    MainNavDestinations.LinkedList -> stringResource(id = R.string.destination_name_linked_list)
-    MainNavDestinations.HashTable -> stringResource(id = R.string.destination_name_hash_table)
-    MainNavDestinations.Favorite -> stringResource(id = R.string.destination_name_favorite)
-    MainNavDestinations.Delete -> stringResource(id = R.string.destination_name_delete)
+    ChooseDataStructureDestination -> stringResource(id = R.string.destination_name_data_structure)
+    FavoritesDestination -> stringResource(id = R.string.destination_name_favorite)
+    DeletedDestination -> stringResource(id = R.string.destination_name_delete)
 }
