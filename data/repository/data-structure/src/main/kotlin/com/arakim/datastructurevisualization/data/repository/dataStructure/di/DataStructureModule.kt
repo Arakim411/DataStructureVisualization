01b2c9bb98cda8420d53ff2a6c052d8dfa34ae7d
barteksawicki <bartlomiej.sawicki@lamin.ar>
2023-11-25 12:51:22 +0100

    fix(Data, DataStructure): binds DataStructureRepositoryImpl with @ActivityRetainedScoped

--- data/repository/data-structure/src/main/kotlin/com/arakim/datastructurevisualization/data/repository/dataStructure/di/DataStructureModule.kt
+++ data/repository/data-structure/src/main/kotlin/com/arakim/datastructurevisualization/data/repository/dataStructure/di/DataStructureModule.kt
@@ -6,12 +6,14 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 
 @Module
 @InstallIn(ActivityRetainedComponent::class)
 internal interface DataStructureModule {
 
     @Binds
+    @ActivityRetainedScoped
     fun bindDataStructureRepository(impl: DataStructureRepositoryImpl): DataStructureRepository
 
 }
