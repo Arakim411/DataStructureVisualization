e03e8df952d5fcc712ec9d9c54ea68a288bd0c17
barteksawicki <bartlomiej.sawicki@lamin.ar>
2023-11-25 12:30:05 +0100

    refactor(Data): rename DataStructureDatabase to AppDatabase and move to separate module

--- data/repository/data-structure/local-data-source/src/main/kotlin/com/arakim/datastrucutrevisualization/data/repository/datastrucutre/localdatasource/Const.kt
+++ data/repository/data-structure/local-data-source/src/main/kotlin/com/arakim/datastrucutrevisualization/data/repository/datastrucutre/localdatasource/Const.kt
@@ -1,6 +1,6 @@
 package com.arakim.datastrucutrevisualization.data.repository.datastrucutre.localdatasource
 
-internal object Const{
+object Const{
     const val DataStructureDataBaseName = "data_structure_db"
     const val DataStructureEntityName = "data_structure"
 }
